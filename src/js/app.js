ae9851df04aacc49967a163aa030f137d5ed2ce2
alinagaripova <alinagaripova25@yandex.ru>
2019-03-27 15:36:01 +0300

    создается окно чата

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,12 @@ import {checkDialogues} from "./valid.js";
 const startChatEl = document.querySelector('.start-chat');
 const dialogueListEl = document.querySelector('.dialogues-list');
 const companionEl = document.querySelectorAll('.companion');
+const chatEl = document.querySelector('.chat');
 
 const companions = Array.from(companionEl);
 const dialogueList = new DialogueList();
 
-console.log(companionEl);
-console.log(companionEl[0]);
-
-//todo: окно чата, отправка сообщений
+//todo: отправка сообщений
 
 let id = 0;
 for (const companion of companions) {
@@ -24,21 +22,21 @@ for (const companion of companions) {
     const image = companion.children[0].attributes[0].textContent;
     const dialogue = new Dialogue(name, image, id);
 
-    companion.addEventListener('click', (evt) => {       //при клике создается новый чат
+    companion.addEventListener('click', (evt) => {      //при клике создается новый чат
         let id = companion.attributes[1].value;
         console.log(id);
         if (checkDialogues(id, dialogueList) > 0) {                 //проверяет существует ли такой чат
             console.log('Такой чат уже существует');
         } else {
-            dialogueList.add(dialogue);
+            dialogueList.add(dialogue);                             //добавляет чат в список чатов
         }
         rebuildTree(dialogueListEl, dialogueList);
     })
 }
 
-function rebuildTree(dialogueListEl, dialogueList) {                //пересобирает дерево
+function rebuildTree(dialogueListEl, dialogueList1) {                //пересобирает дерево
     dialogueListEl.innerHTML = ' ';
-    for (const item of dialogueList.items) {
+    for (const item of dialogueList1.items) {
         const liEl = document.createElement('li');
         liEl.setAttribute('data-class', 'dialogues-element');
         liEl.setAttribute('data-id', item.id);
@@ -46,8 +44,35 @@ function rebuildTree(dialogueListEl, dialogueList) {                //перес
           <img src="${item.image}"><span>${item.name}</span>
         `;
         dialogueListEl.appendChild(liEl);
+
+        liEl.addEventListener('click', () => {
+            createChat(dialogueList, chatEl, item.image, item.name);       //создание окна чата
+        })
     }
 }
+
+function createChat(dialogueList1, chatEl, itemImage, itemName) {       //создание окна чата
+    chatEl.innerHTML = '';
+    const headerEl = document.createElement('header');
+    headerEl.setAttribute('data-class', 'chat-title');
+    headerEl.innerHTML = `
+       <img src="${itemImage}"><span>${itemName}</span>
+    `;
+
+    const footerEl = document.createElement('footer');
+    footerEl.setAttribute('data-class', 'chat-send');
+    footerEl.innerHTML = `
+       <input type="text" placeholder="Введите сообщение">
+       <button id="send">Отправить</button>
+    `;
+    chatEl.appendChild(headerEl);
+    chatEl.appendChild(footerEl);
+
+}
+
+
+
+
 
 rebuildTree(dialogueListEl, dialogueList);
 
--- src/js/lib.js
+++ src/js/lib.js
@@ -7,7 +7,7 @@ export class Dialogue {                         //чат
     }
 }
 
-export class DialogueList { //список чатов
+export class DialogueList {                     //список чатов
     constructor() {
         const savedItems = JSON.parse(localStorage.getItem('DialogueList'));
         if (savedItems !== null) {
@@ -16,13 +16,36 @@ export class DialogueList { //список чатов
             this.items = [];
         }
     }
-
-    add(item) {                                 //добавление элемента в начало списка
+    add(item) {                                 //добавление чата в начало списка чатов
         this.items.unshift(item);
         this.save();
     }
-
     save() {
         localStorage.setItem('DialogueList', JSON.stringify(this.items));
+    }
+}
+
+export class Message {                          //сообщение
+    constructor(name, text, time) {
+        this.name = name;
+        this.text = text;
+        this.time = time;
+    }
+}
+export class MessageList {                     //чат с сообщениями
+    constructor() {
+        const savedItems = JSON.parse(localStorage.getItem('MessageList'));
+        if (savedItems !== null) {
+            this.items = savedItems;
+        } else {
+            this.items = [];
+        }
+    }
+    add(item) {                                 //добавление сообщения в чат
+        this.items.push(item);
+        this.save();
+    }
+    save() {
+        localStorage.setItem('MessageList', JSON.stringify(this.items));
     }
 }
